016fd28d34fe10ed4cb892566288c40b11af46a4
apoorv569 <apoorvs569@gmail.com>
2021-12-14 00:36:56 +0530

    fixup! Caught an unhandled exception from libsndfile

--- src/GUI/MainFrame.hpp
+++ src/GUI/MainFrame.hpp
@@ -183,6 +183,9 @@ class MainFrame : public wxFrame
         // -------------------------------------------------------------------
         wxLongLong m_LoopA, m_LoopB;
 
+        // -------------------------------------------------------------------
+        wxSystemAppearance m_Theme = wxSystemSettings::GetAppearance();
+
     private:
         // -------------------------------------------------------------------
         bool bAutoplay = false;
@@ -258,6 +261,10 @@ class MainFrame : public wxFrame
         // -------------------------------------------------------------------
         // Frame resize event handler
         void OnResizeFrame(wxSizeEvent& event);
+
+        // Splitter window sash pos event handler
+        void OnTopSplitterSashPosChanged(wxSplitterEvent& event);
+        void OnBottomSplitterSashPosChanged(wxSplitterEvent& event);
 
         // -------------------------------------------------------------------
         // Timer update event handler
